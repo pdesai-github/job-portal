d108005ce970090f3e5c8c133cbf4581713445fb
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-11-30 16:06:16 +0530

    User api integrated

---
+++ src/home/models/user.model.ts
@@ -0,0 +1,6 @@
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    createdAt: Date; // Change to Date type for proper handling
+}
--- src/home/widgets/ProfileWidget.tsx
+++ src/home/widgets/ProfileWidget.tsx
@@ -1,16 +1,38 @@
+import { useEffect, useState } from "react";
 import ImageComponent from "../ImageTest";
+import { User } from "../models/user.model";
 
 const ProfileWidget: React.FC = () => {
+
+    const [users, setUsers] = useState<User[]>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:8080/users");
+            const data = await response.json();
+            console.log("fetchData : ", data);
+            setUsers(data)
+        }
+
+        fetchData();
+    });
+
     return (
         <div>
             <div className="card -flex flex-row p-2">
-                <div>
-                    <ImageComponent imageName="pradip.jpeg" height={50} width={50}></ImageComponent>
-                </div>
-                <div className="container">
-                    <h4><b>John Doe</b></h4>
-                    <p>Architect & Engineer</p>
-                </div>
+                {users && users.length > 0 ? (
+                    <>
+                        <div>
+                            <ImageComponent imageName="pradip.jpeg" height={75} width={75}></ImageComponent>
+                        </div>
+                        <div className="container">
+                            <h4><b>{users[1].username}</b></h4>
+                            <p>Architect & Engineer</p>
+                        </div>
+                    </>
+                ) : (
+                    <p>Loading user data...</p> // or any fallback UI when users are not available
+                )}
             </div>
         </div>
     );
